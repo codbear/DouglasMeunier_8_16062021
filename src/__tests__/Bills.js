4c827dd1a5448e5ad7ddee8a8d17341c1b1d0c15
codbear <douglas.meunier@openclassrooms.com>
2021-06-22 23:35:47 +0200

    test(bills): cover loading and error state

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,14 +1,26 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import DashboardUI from '../views/DashboardUI';
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
+  describe('When I am on Bills page but it is loading', () => {
+    test('Then, Loading page should be rendered', () => {
+      const html = BillsUI({ loading: true })
       document.body.innerHTML = html
-      //to-do write expect expression
+      expect(screen.getAllByText('Loading...')).toBeTruthy()
     })
+  })
+
+  describe('When I am on Bills page but back-end send an error message', () => {
+    test('Then, Error page should be rendered', () => {
+      const html = BillsUI({ error: 'some error message' })
+      document.body.innerHTML = html
+      expect(screen.getAllByText('Erreur')).toBeTruthy()
+    })
+  })
+
+  describe("When I am on Bills Page", () => {
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
